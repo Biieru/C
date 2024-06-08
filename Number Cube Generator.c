398127b3356e597c6c2601fccabab34b7a2263a5
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2024-06-08 04:59:53 -0300

    Create Number Cube Generator.c

---
+++ Number Cube Generator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+ int number;
+ printf("Enter a number: ");
+ scanf("%d", &number);
+ int cube = number * number * number;
+ printf("The cube of the number is: %d", cube);
+ return 0;
+
+ }
